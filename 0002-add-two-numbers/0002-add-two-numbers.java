1bea01e1fdafe2d14c1ca54d87c6c0c45d95c3ed
seokjeonghyeon <seokjeonghyeon0418@gmail.com>
2025-11-11 20:37:28 +0900

    Time: 1 ms (100%), Space: 46.3 MB (9.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,23 +11,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0); // 결과 연결리스트의 더미 헤드
-        ListNode current = dummyHead;
-        int carry = 0; // 올림수
+        ListNode dummy = new ListNode(9);
+        ListNode current = dummy;
+        int carry = 0;
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int x = (l1 != null) ? l1.val : 0; // l1 값, 없으면 0
-            int y = (l2 != null) ? l2.val : 0; // l2 값, 없으면 0
-            int sum = x + y + carry; // 합 계산
+        while (l1 != null || l2 != null) {
+        
+        int sum = carry;
 
-            carry = sum / 10; // 올림수
-            current.next = new ListNode(sum % 10); // 현재 자리값 추가
-            current = current.next;
-
-            if (l1 != null) l1 = l1.next; // 다음 노드
-            if (l2 != null) l2 = l2.next; // 다음 노드
+        if(l1 != null) {
+            sum += l1.val;
+            l1 = l1.next;
         }
 
-        return dummyHead.next; // 더미 헤드 다음이 실제 결과
+        if (l2 != null) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+
+        carry = sum / 10;
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        }
+
+        if(carry > 0) current.next = new ListNode(carry);
+    
+        return dummy.next;
     }
 }
